3e95e16ab77b651a7ef5287d8a6ceb18bbc4504a
ArtemMasharipov <112805569+ArtemMasharipov@users.noreply.github.com>
2023-01-10 22:04:57 -0200

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,12 @@
+//ДЗ видео 2. Основы синтаксиса JAVASCRIPT. Структура кода. Инструкции. Режим use strict. Уроки JAVASCRIPT с нуля.
+"use strict";
+console.log('Я учу JS');
+//==================================================
+//ДЗ видео 3. Переменные в JAVASCRIPT. Константы. Ключевые слова LET CONST VAR. Уроки JAVASCRIPT с нуля 2020
+let userEyesColor; // 1 task
+//=======================
+let user;
+let userName = 'Вася';
+user = userName;
+console.log(user);// 2 task
+//===================================================
